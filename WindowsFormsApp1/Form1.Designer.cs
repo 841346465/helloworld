10a9729e5b5ab4c8419799e4e6e38a066bafb220
zijian <zijian@outlook.de>
2018-02-28 10:24:46 +0800

    helloworld

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.a菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.b菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.a子菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.a菜单ToolStripMenuItem,
+            this.b菜单ToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1118, 25);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// a菜单ToolStripMenuItem
+			// 
+			this.a菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.a子菜单ToolStripMenuItem});
+			this.a菜单ToolStripMenuItem.Name = "a菜单ToolStripMenuItem";
+			this.a菜单ToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
+			this.a菜单ToolStripMenuItem.Text = "A菜单";
+			// 
+			// b菜单ToolStripMenuItem
+			// 
+			this.b菜单ToolStripMenuItem.Name = "b菜单ToolStripMenuItem";
+			this.b菜单ToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
+			this.b菜单ToolStripMenuItem.Text = "B菜单";
+			// 
+			// a子菜单ToolStripMenuItem
+			// 
+			this.a子菜单ToolStripMenuItem.Name = "a子菜单ToolStripMenuItem";
+			this.a子菜单ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.a子菜单ToolStripMenuItem.Text = "a子菜单";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1118, 619);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem a菜单ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem a子菜单ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem b菜单ToolStripMenuItem;
+	}
+}
+
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var handler = new HttpClientHandler() { UseCookies = true };
+			var client = new HttpClient(handler);
+			client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0");
+			client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+			client.DefaultRequestHeaders.Add("Keep-Alive", "timeout=600");
+
+			string objectid = "36224.23797.57244.46285";
+			string timestamp = getStrCurrentTimestamp();
+
+			var content = new FormUrlEncodedContent(new[]
+						{
+				new KeyValuePair<string, string>("login_name", "shb01"),
+				new KeyValuePair<string, string>("login_password", "1")
+			});
+			var result = client.PostAsync("http://192.168.200.177:8080/enovia/servlet/login", content);
+			//如果不读取，得不到内容相应，读取了又耗时
+			var resultContent = result.Result.Content.ReadAsStringAsync().Result;
+
+			content = new FormUrlEncodedContent(new[]
+						{
+				new KeyValuePair<string, string>("CreateMode", "EBOM"),
+				new KeyValuePair<string, string>("multiPartCreation", "true"),
+				new KeyValuePair<string, string>("suiteKey", "EngineeringCentral"),
+				new KeyValuePair<string, string>("StringResourceFileId", "emxEngineeringCentralStringResource"),
+				new KeyValuePair<string, string>("SuiteDirectory", "engineeringcentral"),
+				new KeyValuePair<string, string>("objectId", objectid),
+				new KeyValuePair<string, string>("ENCBillOfMaterialsViewCustomFilter", "xxxx"),
+				new KeyValuePair<string, string>("password", "Engineering"),
+				new KeyValuePair<string, string>("timeStamp", timestamp),
+				new KeyValuePair<string, string>("password", "xxxx")
+			});
+
+			var postresult = client.PostAsync("http://192.168.200.177:8080/enovia/same/sameExportBom.jsp", content);
+			//如果不读取，得不到内容相应，读取了又耗时
+			resultContent = postresult.Result.Content.ReadAsStringAsync().Result;
+			var downloaduri = Regex.Match(resultContent, "(?<=action=\"sameDownloadFile.jsp\\?docName=)\\S+(?=\")");
+
+			var finalresult = client.GetStreamAsync("http://192.168.200.177:8080/enovia/same/sameDownloadFile.jsp?docName=" + downloaduri);
+
+			//新建文件流，写入磁盘
+			string path = getSavePath(downloaduri.Value);
+			FileStream fs = new FileStream(path, FileMode.Create);
+
+			byte[] buffer = new byte[1024];
+			int i = 0;//不定义i会使文件大小为1024的整数，即文件末尾填0补齐，会破坏文件格式
+			while ((i = finalresult.Result.Read(buffer, 0, 1024)) > 0)
+			{
+				fs.Write(buffer, 0, i);
+				buffer = new byte[1024];
+			}
+			//关闭文件流 网络流
+			fs.Close();
+			finalresult.Result.Close();
+		}
+
+		public static string getStrCurrentTimestamp()
+		{
+			System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+			TimeSpan ts = DateTime.Now - startTime;
+			//Unix时间戳是秒数，Java时间戳取毫秒数
+			return ts.TotalMilliseconds.ToString("f0");
+		}
+
+		public string getSavePath(string filname)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.FileName = filname;
+			sfd.Filter = @"Excel文件|*.xls";
+			sfd.ShowDialog();
+			return sfd.FileName;
+		}
+	}
+}
